fe60c5d0cc9b4f5da20fcbea4ee190d3770bd2b0
Ryan Lapeyre <ryanlapeyre@gmail.com>
2017-07-25 02:54:41 -0500

    Add project files.

---
+++ PaintApplication/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PaintApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum MyShape
+        {
+            Line, Ellipse, Rectangle
+        }
+        private MyShape currentShape = MyShape.Line;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LineButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentShape = MyShape.Line;
+        }
+
+        private void EllipseButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentShape = MyShape.Ellipse;
+        }
+
+        private void RectangleButton_Click(object sender, RoutedEventArgs e)
+        {
+            currentShape = MyShape.Rectangle;
+        }
+
+
+        Point start;
+        Point end;
+
+        private void MyCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            start = e.GetPosition(this);
+        }
+
+        private void MyCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            switch (currentShape)
+            {
+                case MyShape.Line:
+                    DrawLine();
+                    break;
+                case MyShape.Ellipse:
+                    DrawEllipse();
+                    break;
+                case MyShape.Rectangle:
+                    DrawRectangle();
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private void MyCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                end = e.GetPosition(this);
+            }
+        }
+
+        private void DrawLine()
+        {
+            Line newLine = new Line()
+            {
+                Stroke = Brushes.Blue,
+                X1 = start.X,
+                Y1 = start.Y - 50,
+                X2 = end.X,
+                Y2 = end.Y - 50
+            };
+            MyCanvas.Children.Add(newLine);
+        }
+
+        private void DrawEllipse()
+        {
+            Ellipse newEllipse = new Ellipse()
+            {
+                Stroke = Brushes.Green,
+                Fill = Brushes.Red,
+                StrokeThickness = 4,
+                Height = 10,
+                Width = 10,
+            };
+
+            if(end.X >= start.X)
+            {
+                newEllipse.SetValue(Canvas.LeftProperty, start.X);
+                newEllipse.Width = end.X - start.X;
+            }
+            else
+            {
+                newEllipse.SetValue(Canvas.LeftProperty, end.X);
+                newEllipse.Width = start.X - end.X;
+            }
+
+            if (end.Y >= start.Y)
+            {
+                newEllipse.SetValue(Canvas.LeftProperty, start.Y - 50);
+                newEllipse.Height = end.Y - start.Y;
+            }
+            else
+            {
+                newEllipse.SetValue(Canvas.TopProperty, end.Y - 50);
+                newEllipse.Height = start.Y - end.Y;
+            }
+            MyCanvas.Children.Add(newEllipse);
+        }
+
+        private void DrawRectangle()
+        {
+            Rectangle newRectange = new Rectangle()
+            {
+                Stroke = Brushes.Green,
+                Fill = Brushes.Red,
+                StrokeThickness = 4,
+            };
+
+            if (end.X >= start.X)
+            {
+                newRectange.SetValue(Canvas.LeftProperty, start.X);
+                newRectange.Width = end.X - start.X;
+            }
+            else
+            {
+                newRectange.SetValue(Canvas.LeftProperty, end.X);
+                newRectange.Width = start.X - end.X;
+            }
+
+            if (end.Y >= start.Y)
+            {
+                newRectange.SetValue(Canvas.LeftProperty, start.Y - 50);
+                newRectange.Height = end.Y - start.Y;
+            }
+            else
+            {
+                newRectange.SetValue(Canvas.TopProperty, end.Y - 50);
+                newRectange.Height = start.Y - end.Y;
+            }
+            MyCanvas.Children.Add(newRectange);
+        }
+
+
+    }
+}
