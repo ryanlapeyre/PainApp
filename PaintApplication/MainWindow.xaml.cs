dc7dfbb062d6afba392d025e4bee427b922c396e
Ryan Lapeyre <ryanlapeyre@gmail.com>
2017-07-28 22:40:36 -0500

    bunch of stuff

--- PaintApplication/MainWindow.xaml.cs
+++ PaintApplication/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -22,35 +26,43 @@ namespace PaintApplication
     {
         private enum MyShape
         {
-            Line, Ellipse, Rectangle
+            Line, Ellipse, Rectangle, Brush, Erase, RemoveShapes, Select
         }
         private MyShape currentShape = MyShape.Line;
 
-
+        private Boolean comboClosed = true;
+        private int strokeThickness = 4;
 
         public MainWindow()
         {
             InitializeComponent();
+            this.MyCanvas.EditingMode = InkCanvasEditingMode.None;
         }
+
 
         private void LineButton_Click(object sender, RoutedEventArgs e)
         {
+            this.MyCanvas.EditingMode = InkCanvasEditingMode.None;
             currentShape = MyShape.Line;
         }
 
         private void EllipseButton_Click(object sender, RoutedEventArgs e)
         {
+            this.MyCanvas.EditingMode = InkCanvasEditingMode.None;
             currentShape = MyShape.Ellipse;
         }
 
         private void RectangleButton_Click(object sender, RoutedEventArgs e)
         {
+            this.MyCanvas.EditingMode = InkCanvasEditingMode.None;
             currentShape = MyShape.Rectangle;
         }
 
 
         Point start;
         Point end;
+
+
 
         private void MyCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -69,6 +81,13 @@ namespace PaintApplication
                     break;
                 case MyShape.Rectangle:
                     DrawRectangle();
+                    break;
+                case MyShape.Brush:
+                    break;
+                case MyShape.Erase:
+                    this.MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    break;
+                case MyShape.RemoveShapes:
                     break;
                 default:
                     return;
@@ -91,7 +110,8 @@ namespace PaintApplication
                 X1 = start.X,
                 Y1 = start.Y - 50,
                 X2 = end.X,
-                Y2 = end.Y - 50
+                Y2 = end.Y - 50,
+                StrokeThickness = strokeThickness
             };
             MyCanvas.Children.Add(newLine);
         }
@@ -100,70 +120,253 @@ namespace PaintApplication
         {
             Ellipse newEllipse = new Ellipse()
             {
-                Stroke = Brushes.Green,
                 Fill = Brushes.Red,
-                StrokeThickness = 4,
+                StrokeThickness = strokeThickness,
                 Height = 10,
                 Width = 10,
             };
 
-            if(end.X >= start.X)
+            if (end.X >= start.X)
             {
-                newEllipse.SetValue(Canvas.LeftProperty, start.X);
+                newEllipse.SetValue(InkCanvas.LeftProperty, start.X);
                 newEllipse.Width = end.X - start.X;
             }
             else
             {
-                newEllipse.SetValue(Canvas.LeftProperty, end.X);
+                newEllipse.SetValue(InkCanvas.LeftProperty, end.X);
                 newEllipse.Width = start.X - end.X;
             }
 
             if (end.Y >= start.Y)
             {
-                newEllipse.SetValue(Canvas.LeftProperty, start.Y - 50);
+                newEllipse.SetValue(InkCanvas.LeftProperty, start.Y - 50);
                 newEllipse.Height = end.Y - start.Y;
             }
             else
             {
-                newEllipse.SetValue(Canvas.TopProperty, end.Y - 50);
+                newEllipse.SetValue(InkCanvas.TopProperty, end.Y - 50);
                 newEllipse.Height = start.Y - end.Y;
             }
             MyCanvas.Children.Add(newEllipse);
+
         }
 
         private void DrawRectangle()
         {
-            Rectangle newRectange = new Rectangle()
+            Rectangle newRectangle = new Rectangle()
             {
-                Stroke = Brushes.Green,
                 Fill = Brushes.Red,
-                StrokeThickness = 4,
+                StrokeThickness = strokeThickness,
             };
 
             if (end.X >= start.X)
             {
-                newRectange.SetValue(Canvas.LeftProperty, start.X);
-                newRectange.Width = end.X - start.X;
+                newRectangle.SetValue(InkCanvas.LeftProperty, start.X);
+                newRectangle.Width = end.X - start.X;
             }
             else
             {
-                newRectange.SetValue(Canvas.LeftProperty, end.X);
-                newRectange.Width = start.X - end.X;
+                newRectangle.SetValue(InkCanvas.LeftProperty, end.X);
+                newRectangle.Width = start.X - end.X;
             }
 
             if (end.Y >= start.Y)
             {
-                newRectange.SetValue(Canvas.LeftProperty, start.Y - 50);
-                newRectange.Height = end.Y - start.Y;
+                newRectangle.SetValue(InkCanvas.LeftProperty, start.Y - 50);
+                newRectangle.Height = end.Y - start.Y;
             }
             else
             {
-                newRectange.SetValue(Canvas.TopProperty, end.Y - 50);
-                newRectange.Height = start.Y - end.Y;
+                newRectangle.SetValue(InkCanvas.TopProperty, end.Y - 50);
+                newRectangle.Height = start.Y - end.Y;
             }
-            MyCanvas.Children.Add(newRectange);
+            MyCanvas.Children.Add(newRectangle);
+
+        }
+
+
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            string radioButtonPressed = radioButton.Content.ToString();
+
+            if (radioButtonPressed == "Draw")
+            {
+                this.MyCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                currentShape = MyShape.Brush;
+            }
+            else if (radioButtonPressed == "Erase")
+            {
+                this.MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                currentShape = MyShape.Erase;
+            }
+            else if (radioButtonPressed == "Select")
+            {
+                this.MyCanvas.EditingMode = InkCanvasEditingMode.Select;
+                currentShape = MyShape.Select;
+            }
+            if (radioButtonPressed == "RemoveShape")
+            {
+                this.MyCanvas.EditingMode = InkCanvasEditingMode.None;
+                currentShape = MyShape.RemoveShapes;
+            }
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                 saveFileDialog.DefaultExt = ".bmp";
+                 saveFileDialog.Filter = "Bitmap Image (*.bmp)|*.bmp";
+                 saveFileDialog.ShowDialog();
+                 if (!string.IsNullOrEmpty(saveFileDialog.FileName))
+                 {
+                     SaveCanvas(this, this.MyCanvas, 96, saveFileDialog.FileName);
+                 }
+
+           // this.MyCanvas.Strokes.Save(fs);
+
+            /*
+                   SaveFileDialog saveDialog = new SaveFileDialog();
+                   saveDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+                   if (saveDialog.ShowDialog().Value == true)
+                   {
+                       RenderTargetBitmap targetBitmap =
+                           new RenderTargetBitmap((int)MyCanvas.ActualWidth,
+                                                  (int)MyCanvas.ActualHeight,
+                                                  96d, 96d,
+                                                  PixelFormats.Default);
+                       targetBitmap.Render(MyCanvas);
+
+                       BitmapEncoder encoder = new BmpBitmapEncoder();
+                       string extension = saveDialog.FileName.Substring(saveDialog.FileName.LastIndexOf('.'));
+                       switch (extension.ToLower())
+                       {
+                           case ".jpg":
+                               encoder = new JpegBitmapEncoder();
+                               break;
+                           case ".bmp":
+                               encoder = new BmpBitmapEncoder();
+                               break;
+                           case ".gif":
+                               encoder = new GifBitmapEncoder();
+                               break;
+                           case ".png":
+                               encoder = new PngBitmapEncoder();
+                               break;
+                       }
+                       encoder.Frames.Add(BitmapFrame.Create(targetBitmap));
+                       using (FileStream fs = File.Open(saveDialog.FileName, FileMode.OpenOrCreate))
+                       {
+                           encoder.Save(fs);
+                       }
+
+
+                   }
+                   */
+        }
+
+            public static void SaveCanvas(Window window, InkCanvas canvas, int dpi, string filename)
+            {
+               Size size = new Size(window.Width, window.Height);
+               canvas.Measure(size);
+               //canvas.Arrange(new Rect(size));
+
+               var rtb = new RenderTargetBitmap(
+                   (int)window.Width, //width 
+                   (int)window.Height, //height 
+                   dpi, //dpi x 
+                   dpi, //dpi y 
+                   PixelFormats.Pbgra32 // pixelformat 
+                   );
+               rtb.Render(canvas);
+
+               SaveRTBAsPNG(rtb, filename);
+           }
+
+           private static void SaveRTBAsPNG(RenderTargetBitmap bmp, string filename)
+           {
+               var enc = new System.Windows.Media.Imaging.PngBitmapEncoder();
+               enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(bmp));
+
+               using (var stm = System.IO.File.Create(filename))
+               {
+                   enc.Save(stm);
+               }
+           }
+
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*  try
+              {
+                  OpenFileDialog openDialog = new OpenFileDialog();
+                  if (openDialog.ShowDialog().Value == true)
+                  {
+                      
+                      picture.Source = new BitmapImage(new Uri(openDialog.FileName));
+                      MyCanvas.Width = picture.Width;
+                      MyCanvas.Height = picture.Height;
+                  }
+
+              }
+              catch (Exception ex)
+              {
+
+                  return;
+              }
+              */
+            Image picture = new Image();
+            OpenFileDialog openDialog = new OpenFileDialog();
+            if (openDialog.ShowDialog().Value == true)
+            {
+                picture.Source = new BitmapImage(new Uri(openDialog.FileName));
+               // MyCanvas.Width = picture.Width;
+               // MyCanvas.Height = picture.Height;
+               // MyCanvas.Background = picture;
+                MyCanvas.Children.Add(picture);
+            }
+        }
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+           if (currentShape == MyShape.RemoveShapes)
+            {
+                if (e.OriginalSource is Shape)
+                {
+                    MyCanvas.Children.Remove(e.OriginalSource as UIElement);
+                }
+                
+           }
+        }
+
+        private void BrushSize_DropDownClosed(object sender, EventArgs e)
+        {
+            if (comboClosed)
+            {
+                ChangeBrushSize();
+            }
+            comboClosed = true;
+
+        }
+
+        private void ChangeBrushSize()
+        {
+            try
+            {
+                string brushSize = comboBrushThickness.Text;
+                MessageBox.Show(brushSize);
+
+                strokeThickness = int.Parse(brushSize);
+                strokeAttr.Width = strokeAttr.Height = strokeThickness;
+            }
+            catch
+            {
+
+            }
         }
 
 
     }
+
 }
